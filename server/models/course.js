90d57aa2d65b023266b7c6384e34f0fd205a615e
Mufaddal <mufaddaltopiwala15@gmail.com>
2025-02-14 20:56:16 +0530

    created courses schema

---
+++ server/models/course.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+
+const courseSchema = new mongoose.Schema(
+  {
+    courseTitle: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    lecture: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lecture",
+      },
+    ],
+    quiz: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Lecture",
+      },
+    ],
+    difficulty: {
+      type: String,
+      enum: ["beginner", "intermediate", "advanced"],
+      required: true,
+    },
+    studentsEnrolled: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    isPublished: {
+      type: Boolean,
+      default: false,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Course = mongoose.model("Courses", courseSchema);
