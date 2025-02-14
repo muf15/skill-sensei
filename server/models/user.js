7e1b04b0f95cb5539c633b88973fe2b1760e7f54
Mufaddal <mufaddaltopiwala15@gmail.com>
2025-02-14 20:12:38 +0530

    corrected syntax

--- server/models/user.js
+++ server/models/user.js
@@ -1,32 +1,38 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        required: true,
-        unique: true
+    email: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    password:{
-        type: String,
-        required: true
+    password: {
+      type: String,
+      required: true,
     },
     role: {
-        type: String,
-        enum: ['instructor', 'student'],
-        required: true
+      type: String,
+      enum: ["instructor", "student"],
+      required: true,
     },
-    courses: [{
+    courses: [
+      {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Courses' 
-    }],
-    skills: [{
-        type: String
-    }]
-}, { timestamps: true });
-});
+        ref: "Courses",
+      },
+    ],
+    skills: [
+      {
+        type: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
