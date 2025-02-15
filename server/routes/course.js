e66034ca5092b345558e28f677e4e435196b6d54
Mufaddal <mufaddaltopiwala15@gmail.com>
2025-02-15 15:28:24 +0530

    commented auth middleware import

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,28 +1,28 @@
 import express from "express";
-import { 
-  getAllCourses, 
-  getCourseById, 
-  getLectureById, 
-  getLectureQuiz, 
-  getModuleQuiz 
+import {
+  getAllCourses,
+  getCourseById,
+  getLectureById,
+  getLectureQuiz,
+  getModuleQuiz,
 } from "../controllers/courseController.js";
-import { authMiddleware } from "../middleware/authMiddleware.js"; 
+// import { authMiddleware } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // Get all courses
-router.get("/", getAllCourses); 
+router.get("/", getAllCourses);
 
 // Get a specific course by ID
-router.get("/:courseId",getCourseById);
+router.get("/:courseId", getCourseById);
 
 // Get a specific lecture inside a course
 router.get("/:courseId/:lectureId", getLectureById);
 
 // Get quiz for a specific lecture
-router.get("/:courseId/:lectureId/:quizId",getLectureQuiz);
+router.get("/:courseId/:lectureId/:quizId", getLectureQuiz);
 
 // Get module quiz
-router.get("/:courseId/mquiz",getModuleQuiz);
+router.get("/:courseId/mquiz", getModuleQuiz);
 
 export default router;
--- server/routes/instructor.js
+++ server/routes/instructor.js
@@ -1,5 +1,5 @@
 import express from "express";
-import isAuthenticated from "../middleware/isAuthenticated.js";
+// import isAuthenticated from "../middleware/isAuthenticated.js";
 import { createCourse, createLecture, editCourse, editLecture, getCourseById, getCourseLecture, getCreatorCourses, getLectureById, getPublishedCourse, removeLecture, searchCourse, togglePublishCourse } from "../controllers/instructorController.js";
 import upload from "../utils/multer.js";
 const router = express.Router();
